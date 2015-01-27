99a583d7efa4ae7f271cc1aae1d453ac1379169d
Linnea <lkdamer@gmail.com>
2015-01-27 14:36:28 -0800

    sets associations for MedicalConditions class

--- app/models/medical_condition.rb
+++ app/models/medical_condition.rb
@@ -1,2 +1,9 @@
 class MedicalCondition < ActiveRecord::Base
+  has_one :primary_prevention
+  has_many :possible_treatments
+  has_many :medical_condition_medical_codes
+  has_many :medical_codes, through: :medical_condition_medical_codes
+  has_many :medical_condition_medical_causes
+  has_many :medical_causes, through: :medical_condition_medical_causes
+
 end
