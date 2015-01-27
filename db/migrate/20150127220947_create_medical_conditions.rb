10bdad5353cf5693c367ca4a23c617286e2cd3b7
Linnea <lkdamer@gmail.com>
2015-01-27 14:10:03 -0800

    generate MedicalCondition model

---
+++ db/migrate/20150127220947_create_medical_conditions.rb
@@ -0,0 +1,10 @@
+class CreateMedicalConditions < ActiveRecord::Migration
+  def change
+    create_table :medical_conditions do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
