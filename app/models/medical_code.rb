ca3e635cb2cf54da364cceae573539b930a4aebb
Linnea <lkdamer@gmail.com>
2015-01-30 10:16:17 -0800

    added validation of code's uniqueness within a code system

--- app/models/medical_code.rb
+++ app/models/medical_code.rb
@@ -1,5 +1,5 @@
 class MedicalCode < ActiveRecord::Base
   has_one :medical_code_condition
   has_one :medical_condition, through: :medical_code_conditions
-  # validates :medical_code_condition, presence: true
+  validates_uniqueness_of :code_value, scope: :code_system
 end
