8460ea8f90a52a76d8401a4eb529b2f08e610e6e
Linnea <lkdamer@gmail.com>
2015-01-30 14:08:38 -0800

    comments in validator

--- app/models/VMedicalCodeCondition_validator.rb
+++ app/models/VMedicalCodeCondition_validator.rb
@@ -1,9 +1,16 @@
 class VMedicalCodeConditionValidator < ActiveModel::EachValidator
 
   def validate_each record, attribute, value
-    # this method validates that
+    # this method validates that a medical condition does not get more than one code
+    # from the same organization
+
+    # codes that the medical condition has already been assigned
     codes = MedicalCondition.find(record.medical_condition_id)
+
+    # the code systems of those codes
     organizations = codes.map {|code| code.code_system}
+
+    # test if the new code is from a system that has already submitted a code
     if organizations.include? MedicalCode.find(value).code_system
       record.errors[attribute] << (
         options[:message] || "is from an organization that has already assigned a code to this disease"
