683d0d205e86dd9c5eb54404b941721fe119a770
Linnea <lkdamer@gmail.com>
2015-02-02 17:14:30 -0800

    wrote second test for medical code condition validation

--- spec/models/medical_code_condition_spec.rb
+++ spec/models/medical_code_condition_spec.rb
@@ -11,5 +11,15 @@ describe MedicalCodeCondition do
       y = MedicalCode.create(code_system: "Linnea's Diseases", code_value: "1")
       expect(MedicalCodeCondition.new(medical_code_id: y.id, medical_condition_id: d.id)).to be_invalid
     end
+
+    it 'passes if you add a code from a new system' do
+      d = MedicalCondition.create(
+        name: "Hypochondria",
+        description: "Subject has every disease, ever"
+      )
+      d.codes.create(code_system: "Linnea's Diseases", code_value: "1234woo")
+      y = MedicalCode.create(code_system: "Brenda's Diseases", code_value: '1234hoo')
+      expect(MedicalCodeCondition.new(medical_code_id: y.id, medical_condition_id: d.id)).to be_valid
+    end
   end
 end
