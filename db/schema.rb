7d2a1081f354e019c7259d0213351d41ccfa2e26
Linnea <lkdamer@gmail.com>
2015-02-03 10:35:11 -0800

    created migration to make tier default to unconfirmed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203045948) do
+ActiveRecord::Schema.define(version: 20150203183145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 20150203045948) do
     t.string   "name"
     t.string   "password"
     t.string   "email"
-    t.string   "tier"
+    t.string   "tier",       default: "unconfirmed"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
