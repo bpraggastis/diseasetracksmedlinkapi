2c7115f0571194e8029d68b1406bc4a1452086d7
Linnea <lkdamer@gmail.com>
2015-02-03 11:02:59 -0800

    added security checks to show, update, delete methods, and authorization_check function.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,20 +12,41 @@ class UsersController < ApplicationController
 
   def show
     #Only logged-in users can view their own pages
+    unless params[:id] == session[:session_id]
+      flash[:notice] = "You are not authorized to see that page."
+      redirect_to root_path
+    end
     @user = User.find(session[:session_id])
   end
 
   def update
     #This allows users to update their own info, and admins to update tier status
-    @user = User.find(params[:user_id])
+    unless authorization_check
+      flash[:notice] = "You are not authorized to update that user's profile."
+      redirect_to root_path
+    end
+    @user = User.find(params[:id])
   end
 
   def delete
     #This allows users to delete themselves, or admins to delete them.
+    unless authorization_check
+      flash[:notice] = "You are not authorized to delete that user."
+      redirect_to root_path
+    end
     @user = User.find(params[:user_id])
+    @user.delete
+    #Do we want to delete their comments and queries as well?
+    #Do we want to email them these things first?
+    flash[:notice] = "User successfully deleted."
+    redirect_to root_path
   end
 
   private
+
+  def authorization_check
+    params[:id] == session[:session_id] || User.find(session[:session_id]).tier == "admin"
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
