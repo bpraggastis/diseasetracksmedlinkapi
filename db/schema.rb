4e69e2b15d08c3067a56ae6efa9a305b12002095
Linnea <lkdamer@gmail.com>
2015-02-03 10:14:46 -0800

    generated user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150203000002) do
+ActiveRecord::Schema.define(version: 20150203045948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,15 @@ ActiveRecord::Schema.define(version: 20150203000002) do
   create_table "synonyms", force: true do |t|
     t.integer  "medical_condition_id"
     t.integer  "alternate_name_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "password"
+    t.string   "email"
+    t.string   "tier"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
