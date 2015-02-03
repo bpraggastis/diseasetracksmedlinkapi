57b1dc75eba4f5bb828673e1839ba050a999ca5b
Linnea <lkdamer@gmail.com>
2015-02-02 17:01:38 -0800

    rewrote test to explicitly test the join table entry

--- spec/models/medical_code_condition_spec.rb
+++ spec/models/medical_code_condition_spec.rb
@@ -8,7 +8,8 @@ describe MedicalCodeCondition do
         description: "Subject has every disease, ever"
       )
       d.codes.create(code_system: "Linnea's Diseases", code_value: "1234woo")
-      expect(d.codes.new(code_system: "Linnea's Diseases", code_value: "1")).to be_invalid
+      y = MedicalCode.create(code_system: "Linnea's Diseases", code_value: "1")
+      expect(MedicalCodeCondition.new(medical_code_id: y.id, medical_condition_id: d.id)).to be_invalid
     end
   end
 end
