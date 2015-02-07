5cfea1379326c2a85db3046934b7dd1fe93a3089
Brenda Praggastis <bpraggastis@gmail.com>
2015-02-06 23:28:56 -0800

    looks like it is seeding from diseasome but need a bigger datasample to check for certain.

--- app/models/alternate_name.rb
+++ app/models/alternate_name.rb
@@ -2,8 +2,8 @@ class AlternateName < ActiveRecord::Base
 
   include Searchable
 
-  has_many :synonyms
-  has_many :medical_conditions, through: :synonyms
+  has_one :synonym
+  has_one :medical_condition, through: :synonyms
 
   def self.search(query)
     response = __elasticsearch__.search(
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,101 +6,117 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# DISEASE_DATA = JSON.parse(File.read('db/support/disease_file_short.json'))['diseases']
-# diseases = MedicalConditionHelpers::DataSeed.make_disease_bank(DISEASE_DATA)
-#
-# diseases.each do |disease|
-#   new_disease = MedicalCondition.create(name: disease['name'])
-#   # taken from rdfs:label
-#     disease["alternate_names"].each do |alternate_name|
-#       new_disease.alternate_names.create(name: alternate_name)
-#     end
-#   # Faker data used for causes
-#   rand(3).times do
-#     new_disease.causes.create(name: Faker::Lorem.word, description: Faker::Company.catch_phrase)
-#   end
-#
-#   disease["codes"].each do |code|
-#     new_disease.codes.create(code_system: code["system"], code_value: code["value"])
-#   end
-# end
-#
-# DRUG_DATA = Nokogiri::XML(File.read('db/support/drug_data.xml'))
-# drugs = DRUG_DATA.css('/drugbank/drug')
-# puts drugs.length
-# n=0
-# drugs.each do |drug|
-#   name = drug.css("/name").text
-#   puts drugs.length - n
-#   n += 1
-#   description = drug.css('/description').text
-#   d = MedicalTherapy.create(name: name, description: description)
-#   d.codes.create(code_value: drug.css('cas-number').text, code_system: "cas-number")
-#   d.codes.create(code_system: 'DrugBank', code_value: drug.css('/drugbank-id').first.text)
-#   codes = drug.css('/external-identifiers/external-identifier')
-#   codes.each do |code|
-#     d.codes.create(code_system: code.css('/resource').text, code_value: code.css('identifier').text)
-#   end
-# end
-#
-#
-# DMED = MedicalTherapyHelpers::DailyMedSeed::make_daily_med_seed('db/support/dmedsmall.json')
-# # This returns {dmedcode => {name:----, db_code:----, generic:----, description:----},--=>{..}...}
-# # Check medical_therapy_helpers for additional fields
-#
-# DMED.keys.each do |dkey|  #dkey = primary key for DailyMed record
-#   dKey = DMED[dkey]
-#   therapy = nil
-#   therapy_code = MedicalCode.find_by(code_value: dKey[:db_code])
-#   if dKey[:db_code] != nil
-#     therapy = therapy_code.medical_code_therapy.medical_therapy if therapy_code != nil
-#   end
-#   if therapy != nil
-#     therapy.codes.create(code_system: "DailyMed", code_value: dkey)
-#     therapy.therapy_alternate_names.create(name: dKey[:generic])
-#   else
-#     therapy = MedicalTherapy.create(name: dKey[:name], description: dKey[:description])
-#     therapy.therapy_alternate_names.create(name: dKey[:generic]) if dKey[:generic] != nil
-#     therapy.codes.create(code_system: "DrugBank", code_value: dKey[:db_code]) if dKey[:db_code] != nil
-#     therapy.codes.create(code_system: "DailyMed", code_value: dkey)
-#   end
-# end
+DISEASE_DATA = JSON.parse(File.read('db/support/disease_file_short.json'))['diseases']
+
+diseases = MedicalConditionHelpers::DataSeed.make_disease_bank(DISEASE_DATA)
+
+diseases.each do |disease|
+  new_disease = MedicalCondition.create(name: disease['name'])
+  # taken from rdfs:label
+    disease["alternate_names"].each do |alternate_name|
+      new_disease.alternate_names.create(name: alternate_name)
+    end
+  # Faker data used for causes
+  rand(3).times do
+    new_disease.causes.create(name: Faker::Lorem.word, description: Faker::Company.catch_phrase)
+  end
+
+  disease["codes"].each do |code|
+    new_disease.codes.create(code_system: code["system"], code_value: code["value"])
+  end
+end
+
+DRUG_DATA = Nokogiri::XML(File.read('db/support/drug_data.xml'))
+drugs = DRUG_DATA.css('/drugbank/drug')
+puts drugs.length
+n=0
+drugs.each do |drug|
+  name = drug.css("/name").text
+  puts drugs.length - n
+  n += 1
+  description = drug.css('/description').text
+  d = MedicalTherapy.create(name: name, description: description)
+  d.codes.create(code_value: drug.css('cas-number').text, code_system: "cas-number")
+  d.codes.create(code_system: 'DrugBank', code_value: drug.css('/drugbank-id').first.text)
+  codes = drug.css('/external-identifiers/external-identifier')
+  codes.each do |code|
+    d.codes.create(code_system: code.css('/resource').text, code_value: code.css('identifier').text)
+  end
+end
 
 
-l = JSON.parse(File.read('db/support/diseasome_dump.json'))
+DMED = MedicalTherapyHelpers::DailyMedSeed::make_daily_med_seed('db/support/dmedsmall.json')
+# This returns {dmedcode => {name:----, db_code:----, generic:----, description:----},--=>{..}...}
+# Check medical_therapy_helpers for additional fields
+
+DMED.keys.each do |dkey|  #dkey = primary key for DailyMed record
+  dKey = DMED[dkey]
+  therapy = nil
+  therapy_code = MedicalCode.find_by(code_value: dKey[:db_code])
+  if dKey[:db_code] != nil
+    therapy = therapy_code.medical_code_therapy.medical_therapy if therapy_code != nil
+  end
+  if therapy != nil
+    therapy.codes.create(code_system: "DailyMed", code_value: dkey)
+    therapy.therapy_alternate_names.create(name: dKey[:generic])
+  else
+    therapy = MedicalTherapy.create(name: dKey[:name], description: dKey[:description])
+    therapy.therapy_alternate_names.create(name: dKey[:generic]) if dKey[:generic] != nil
+    therapy.codes.create(code_system: "DrugBank", code_value: dKey[:db_code]) if dKey[:db_code] != nil
+    therapy.codes.create(code_system: "DailyMed", code_value: dkey)
+  end
+end
+
+
+# l = JSON.parse(File.read('https://drive.google.com/file/d/0Bxy3dxgL-9bST3pxS3RGRG1oRFk/view?usp=sharing'))
+l = JSON.parse(File.read('db/support/diseasomesmall.json'))
+
 n = l.keys.length
 l.keys.each do |key|
   puts n
   n -= 1
   name = l[key]['http://schema.org/name'][0]['value'].gsub("_", " ") if l[key]['http://schema.org/name']
-  disease = MedicalCondition.where(name: name)
-
-  if l[key]["http://schema.org/primaryPrevention"]
-    l[key]["http://schema.org/primaryPrevention"].each do |hash|
+  preventions = l[key]["http://schema.org/primaryPrevention"]
+  if name != nil && preventions != nil
+    diseasealt = AlternateName.find_by(name: name)
+    if diseasealt != nil
+      disease = diseasealt.synonym.medical_condition
+    else
+      disease = MedicalCondition.create(name: name)
+      disease.alternate_names.create(name: name)
+    end
+    preventions.each do |hash|
       # if nums = hash['value'].match(/http:\/\/beowulf.pnnl.gov\/2014\/drug\/DB\d+/)
-      db, code = parse_drug(hash['value'])
+      dcode = /http:\/\/beowulf\.pnnl\.gov\/2014\/drug\/(DB)*(\d*)/.match(hash['value'])
+      db, code = dcode[1], dcode[2]
       if db != nil
-        drug = MedicalCode.where(
+        drug_code = MedicalCode.find_by(
                               code_system: "DrugBank",
-                              value: db + code
-                            ).medical_code_therapy.medical_therapy
-      else
-        drug = MedicalCode.where(
+                              code_value: db + code
+                            )
+        drug = drug_code.medical_code_therapy.medical_therapy if drug_code != nil
+      elsif
+        drug_code = MedicalCode.find_by(
                               code_system: "DailyMed",
-                              value: code
-                            ).medical_code_therapy.medical_therapy
+                              code_value: code
+                            )
+        drug = drug_code.medical_code_therapy.medical_therapy if drug_code != nil
       end
-      if drug && disease
+      if drug != nil && disease != nil
+        puts "heeeeeere"
         PrimaryPrevention.create(
                             medical_therapy_id: drug.id,
                             medical_condition_id: disease.id)
       else
+        puts '*'*(80)
         puts "#{name} not in db?"
-        puts "drug: " + drug.inspect
-        puts "disease: " + disease.inspect
+        puts "drug: ", dcode
+        puts "disease: " , disease.name
+        puts '*'*(80)
       end
     end
   end
+
 end
 
 
