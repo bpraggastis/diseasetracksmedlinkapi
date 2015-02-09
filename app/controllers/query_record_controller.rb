6c7932b1d9883bdb2847f9b1013f4e5f685a259f
Linnea <lkdamer@gmail.com>
2015-02-08 23:07:05 -0800

    Query Record create method

--- app/controllers/query_record_controller.rb
+++ app/controllers/query_record_controller.rb
@@ -1,11 +1,17 @@
 class QueryRecordController < ApplicationController
 
   def create
-    
+    @record = QueryRecord.new(query_params)
+    if @record.save
+      redirect_to @query
+    else
+      flash[:notice] = "Query record failed to save."
+      redirect_to root_path
+    end
   end
 
   def show
-
+    @record = QueryRecord.find(params[:id])
   end
 
   def destroy
@@ -19,6 +25,13 @@ class QueryRecordController < ApplicationController
       flash[:notice] = "You are not authorized to delete that query."
       redirect_to @user
     end
+  end
+
+  private
+
+  def query_params
+    params.require(:query).permit(:disease, :disease, :start_date,
+                                  :end_date, :location, :user_id)
   end
 
 end
