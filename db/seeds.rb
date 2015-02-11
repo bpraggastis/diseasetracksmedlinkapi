21901ff2694ec285cab4ad9350e3f3aa2e845c36
Linnea <lkdamer@gmail.com>
2015-02-11 09:39:35 -0800

    finished event seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # # # This file should contain all the record creation needed to seed the database with its default values.
 # # # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 # # #
@@ -245,27 +246,27 @@
 #
 #
 # ############# Convert CSV file to JSON ####################
-# def is_int?(string)
-#   !!(string =~ /^[+-]?[1-9][0-9]*$/)
-# end
+def is_int?(string)
+  !!(string =~ /^[+-]?[1-9][0-9]*$/)
+end
 #
 # ####--> Replace with correct path name
-# lines = CSV.open('/Users/brendapraggastis/Ada/capstone/datafiles/us_geo_populated_place.csv').readlines
+# lines = CSV.open('db/support/us_geo_populated_place.csv').readlines
 # keys = lines.shift
-# n= lines.count
+# n = lines.count
 #
-# File.open("/Users/brendapraggastis/Ada/capstone/datafiles/us_locations.json", "w") do |f|
+# File.open("db/support/us_locations.json", "w") do |f|
 #   data = lines.map do |values|
 #           puts n
-#           n -=1
+#           n -= 1
 #           Hash[keys.zip(values.map{|val| is_int?(val) ? val.to_i : val})]
 #           end
 #   f.puts JSON.pretty_generate(data)
 #   f.close
 # end
-#
-#
-# ############## Seed Places Table #############
+# #
+# #
+# # ############## Seed Places Table #############
 # lines = CSV.open('db/support/places.csv').readlines
 # lines.each do |abbr,region|
 #   Place.create(name: region ,abbreviation: abbr)
@@ -275,19 +276,19 @@
 #
 #
 # #####--> Replace with correct path name
-# geo_data = JSON.parse(File.read("/Users/brendapraggastis/Ada/capstone/datafiles/us_locations.json"))
-# geo_data = JSON.parse(File.read('db/support/locations.json'))
-# geo_data.each do |local|
-#   new_geo = Geo.create(
-#               name: local["FEATURE_NAME"],
-#               latitude: local["PRIM_LAT_DEC"],
-#               longitude: local["PRIM_LONG_DEC"],
-#               county: local["COUNTY_NAME"],
-#               place_id: Place.find_by(abbreviation: local["STATE_ALPHA"] ).id
-#               )
-#               puts new_geo.name
-#
-# end
+geo_data = JSON.parse(File.read("db/support/us_locations.json"))
+geo_data = JSON.parse(File.read('db/support/locations.json'))
+geo_data.each do |local|
+  new_geo = Geo.create(
+              name: local["FEATURE_NAME"],
+              latitude: local["PRIM_LAT_DEC"],
+              longitude: local["PRIM_LONG_DEC"],
+              county: local["COUNTY_NAME"],
+              place_id: Place.find_by(abbreviation: local["STATE_ALPHA"] ).id
+              )
+              puts new_geo.name
+
+end
 #
 #
 #
@@ -327,38 +328,21 @@
 # # add counties to Geo data
 # # add place_id to Geo data
 
-outbreak1 = JSON.parse(File.read("db/support/outbreak-1.json"))
+DISEASE_HASH = {
+  "Rubella" => ["D012409","10018206","N0000002655","36653000","C0035920"],
+  "Ebola" => ["D019142","10014071","N0000003898","37109004","C0282687"],
+  "Malaria" => ["1385","7728","C03.752.250.552","Malaria","248310"],
+  "West Nile Virus" => ["C1096184"],
+  "Encephalitis" => ["C0014060"],
+  "Mumps" => ["D009107","10009300","N0000002055","240526004","36989005","C0026780"]
+}
 
-outbreak1.each do |event|
-  e = Event.new()
-  e.date = Date.strptime(event["location"]["DATE_CREATED"], "%m/%d/%y")
-  e.number_infected = event["population"].to_i
-  e.save
+code_hash = {}
+DISEASE_HASH.each do |key,value|
+  value.each do |val|
+    code_hash[val] = key
+  end
 end
-
-outbreak2.each do |event|
-  e = Event.new()
-  e.date = Date.strptime(event["location"]["DATE_CREATED"], "%m/%d/%y")
-  e.number_infected = event["population"].to_i
-  e.save
-end
-puts outbreaks.length
-
-# DISEASE_HASH = {
-#   "Rubella" => ["D012409","10018206","N0000002655","36653000","C0035920"],
-#   "Ebola" => ["D019142","10014071","N0000003898","37109004","C0282687"],
-#   "Malaria" => ["1385","7728","C03.752.250.552","Malaria","248310"],
-#   "West Nile Virus" => ["C1096184"],
-#   "Encephalitis" => ["C0014060"],
-#   "Mumps" => ["D009107","10009300","N0000002055","240526004","36989005","C0026780"]
-# }
-#
-# code_hash = {}
-# DISEASE_HASH.each do |key,value|
-#   value.each do |val|
-#     code_hash[val] = key
-#   end
-# end
 
 ### to enter Events:
 #  event_hash.keys = {number_infected, date, disease_code, latitude,
@@ -372,8 +356,28 @@ puts outbreaks.length
 
 ######## Outbreaks  Seed:##########
 
-# Outbreak.create(title: "Mosquito Born", description: "Cases of Malaria and West " +
-# "Nile Virus found in Louisiana, Florida, Georgia, Mississippi, Arkansas, and Alabama.")
-# Outbreak.create(title: "Under-vaccination", description: "Cases of Rubella, Mumps, and " +
-# "Encephalitis in California, Oregon, and Washington due to lack of vaccination.")
-# Outbreak.create(title: "Ebola", description: "Cases of Ebola occuring in New York State.")
+Outbreak.create(title: "Mosquito Born", description: "Cases of Malaria and West " +
+"Nile Virus found in Louisiana, Florida, Georgia, Mississippi, Arkansas, and Alabama.")
+Outbreak.create(title: "Under-vaccination", description: "Cases of Rubella, Mumps, and " +
+"Encephalitis in California, Oregon, and Washington due to lack of vaccination.")
+Outbreak.create(title: "Ebola", description: "Cases of Ebola occuring in New York State.")
+
+(1..3).each do |n|
+  outbreak = JSON.parse(File.read("db/support/outbreak-#{n}.json"))
+  outbreak.each do |event|
+    unless g = Geo.find_by(name: event["location"]["FEATURE_NAME"])
+        pl = Place.find_by(abbreviation: event["location"]["STATE_ALPHA"])
+        g = Geo.create(latitude: event["location"]["PRIM_LAT_DEC"],
+                   longitude: event["location"]["PRIM_LAT_DEC"],
+                   name: event["location"]["FEATURE_NAME"],
+                   county: event["location"]["COUNTY_NAME"],
+                   place_id: pl.id)
+    end
+    event["location"]["DATE_CREATED"]? d = Date.strptime(event["location"]["DATE_CREATED"], "%m/%d/%y") : d = nil
+    Outbreak.find(n).events.new(
+        date: d,
+        medical_condition_id: MedicalCondition.find_by(name: code_hash[event["disease"]]),
+        number_infected: event["population"].to_i,
+        geo_id: g.id)
+  end
+end
