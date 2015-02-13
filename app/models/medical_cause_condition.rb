9265fcd3aabd3c702ff4144ace8fb94fe7b4c7ec
Brenda Praggastis <bpraggastis@gmail.com>
2015-02-13 14:21:04 -0800

    uncommented all custom validations

--- app/models/medical_cause_condition.rb
+++ app/models/medical_cause_condition.rb
@@ -1,8 +1,8 @@
 class MedicalCauseCondition < ActiveRecord::Base
 
   include Searchable
-  
+
   belongs_to :medical_condition
   belongs_to :medical_cause
-  # validates :medical_cause, :medical_condition, presence: true
+  validates :medical_cause, :medical_condition, presence: true
 end
