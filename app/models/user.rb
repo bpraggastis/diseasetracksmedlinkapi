8d9bccea264b772ee077ef5be862ae244f32e02e
Linnea <lkdamer@gmail.com>
2015-02-12 22:54:31 -0800

    user now validates name and email on update (not password)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :name, :email, :password, presence: true, on: :create
+  validates :name, :email, presence: true, on: :update
   validates :email, :name, uniqueness: true, on: :create, on: :update
   has_secure_password
 
