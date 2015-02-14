4b8d6a2c8c2cdc3d90325e3c94b768240a1c19c8
Brenda Praggastis <bpraggastis@gmail.com>
2015-02-14 14:25:50 -0800

    created info box

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -6,12 +6,11 @@ $(function(){
   function initialize() {
 
     var mapElement = $("#map-canvas");
-    var events = $("meta").attr
 
     var latitude = parseFloat(mapElement.attr('data-center-latitude'));
     var longitude = parseFloat(mapElement.attr('data-center-longitude'));
 
-    var latLng = new google.maps.LatLng(latitude,longitude)
+    var latLng = new google.maps.LatLng(latitude,longitude);
     var mapOptions = {
       center: latLng,
       zoom: 4,
@@ -30,54 +29,86 @@ $(function(){
         position: google.maps.ControlPosition. RIGHT_TOP
       }
     };
-    map = new google.maps.Map(document.getElementById('map-canvas'),
-        mapOptions);
+    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    // var marker = new google.maps.Marker({
-    //   position: new google.maps.LatLng(latitude, longitude),
-    //   map: map
-    // });
+
+
   }
+  // ends initialize
 
-  var make_mark = function(latitude,longitude){
+
+  var content_string = function(my_content){ string = '<h4>'+ my_content + '</h4>' +
+                        '<button href="#" + class="remove-marker">Remove Marker</button>';
+                        return string;
+  };
+
+
+
+  var myInfoWindow = function(my_content, my_marker){
+    var infowindow = new google.maps.InfoWindow({
+      content: content_string(my_content),
+    });
+    console.log(my_marker.__gm_id);
+    infowindow.open(map,my_marker);
+    $('.remove-marker').click(function(my_marker){
+      console.log('made it here');
+      my_marker.setMap(null);
+    });
+  };
+
+  var make_mark = function(event){
+    var latitude = parseFloat(event.attr('data-latitude'));
+    var longitude = parseFloat(event.attr('data-longitude'));
+    var id = parseInt(event.attr('data-event-id'));
+    var disease = event.attr('data-disease');
+
     var marker = new google.maps.Marker({
       position: new google.maps.LatLng(latitude,longitude),
+      event_id: id,
+      event_disease: disease,
     });
+    console.log(marker.event_id, marker.event_disease, marker.__gm_id);
     marker.setMap(map);
     markers.push(marker);
+    // google.maps.event.addListener(marker, 'click',function(){
+    //   console.log("clicked marker");
+    google.maps.event.addListener(marker, 'click', function(e){
+      myInfoWindow(marker.event_disease, marker);
+    });
+
   };
 
   var place_marker = function(e){
     var new_mark = $(e.target);
-    var latitude = parseFloat(new_mark.attr('data-latitude'));
-    var longitude = parseFloat(new_mark.attr('data-longitude'));
-    make_mark(latitude,longitude);
+    make_mark(new_mark);
   };
 
   var auto_marks = function(){
-    var spots = $(".place-marker").children(".location-marker")
+    var spots = $(".place-marker").children(".location-marker");
     spots.each(function(){
-      latitude = parseFloat(this.getAttribute('data-latitude'))
-      longitude = parseFloat(this.getAttribute('data-longitude'))
-      make_mark(latitude,longitude);
+      latitude = parseFloat(this.getAttribute('data-latitude'));
+      longitude = parseFloat(this.getAttribute('data-longitude'));
+      event_id = this.
+      make_mark(this);
     });
   };
 
   var clear_markers = function(){
     for (var i = 0; i < markers.length; i++) {
       markers[i].setMap(null);
-    };
+    }
     markers = [];
-  }
+  };
 
   var remove_marker = function(e){
-    
-    var index = markers.indexOf(e)
-  }
+    var index = markers.indexOf(e);
+  };
+
 
   $(".location-marker").click(place_marker);
   $(".clear-markers").click(clear_markers);
   $(".auto-mark").click(auto_marks);
+  $(".remove-marker").click(remove_marker);
 
   google.maps.event.addDomListener(window, 'load', initialize);
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,31 +2,43 @@ class HomeController < ApplicationController
 
   def index
     @user = User.find(session[:user_id]) if session[:user_id]
-    @outbreaks = Outbreak.all.collect{|x| x.id}
-    @diseases = find_diseases
+    @outbreaks = Outbreak.all.sort_by{|x| x.title}.collect{|x| x.id}
+    @diseases = MedicalCondition.all.sort_by{|x| x.name}.collect{|x| x.id}
     @events = Event.where(
           date: DateTime.parse('Jan. 1, 1960') .. DateTime.now
           ).to_a.sort_by{|event| event.date}.reverse
-
+    @places = Place.all.sort_by{|x| x.name}.collect{|x| x.id}
   end
 
   def query
-    basic_diseases = []
-    (1..3).each do |n|
-      basic_diseases += Outbreak.find(n).medical_conditions.collect{|x| x.id}
-    end # this is a list of ids for all outbreak codes
+    # basic_diseases = []
+    # (1..3).each do |n|
+    #   basic_diseases += Outbreak.find(n).medical_conditions.collect{|x| x.id}
+    # end # this is a list of ids for all outbreak codes
+
     @outbreakq = [params[:outbreak_id].to_i] #ids for queried outbreaks
     @diseaseq = [params[:disease_id].to_i] #ids for queried diseases
+    @statesq = [params[:place_id].to_i] #ids for queried states
+
     @outbreakq.include?(0) ? @outbreaks = Outbreak.all.collect{|x| x.id} : @outbreaks = @outbreakq
     @diseaseq.include?(0)? @diseases = find_diseases(@outbreaks) : @diseases = @diseaseq
-    @events = Event.where({outbreak_id: @outbreaks, medical_condition_id: @diseases }).sort_by{|event| event.date}.reverse
+    @statesq.include?(0)? @places = find_places(@outbreaks): @places = @statesq
+    @events = Event.joins(geo: :place).where({outbreak_id: @outbreaks, medical_condition_id: @diseases,
+                                        places: {id: @places}}).sort_by{|event| event.date}.reverse
+
     render 'home/index'
   end
 
   def find_diseases(array = [1,2,3]) # return ids
     diseases = []
-    array.each{|x| diseases += Outbreak.find(x).medical_conditions.collect{|x| x.id}}
+    array.each{|x| diseases += Outbreak.find(x).medical_conditions.collect{|y| y.id}}
     diseases
+  end
+
+  def find_places(array = [1,2,3]) # return ids
+    states = []
+    array.each{|x| states += Outbreak.find(x).places.collect{|y| y.id}}
+    states
   end
 
     # @cquery = ""
