01646929479b13e649b6ce00972e27eaae40b446
Linnea <lkdamer@gmail.com>
2015-02-18 09:22:15 -0800

    fixed error relating to unknown email address trying to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:email])
-    if @user == @user.authenticate(params[:password])
+    if @user && @user == @user.authenticate(params[:password])
       session[:user_id] = @user.id.to_s
       redirect_to root_path
     else
