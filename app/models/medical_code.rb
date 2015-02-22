0db257fc3cdfcd3805a8e2b76c1b951f8019acf6
Brenda Praggastis <bpraggastis@gmail.com>
2015-02-22 08:46:59 -0800

    changed has_one medical_code_therapy to has_many

--- app/models/medical_code.rb
+++ app/models/medical_code.rb
@@ -1,7 +1,7 @@
 class MedicalCode < ActiveRecord::Base
 
   include Searchable
-  has_one :medical_code_therapy
+  has_many :medical_code_therapies
   has_many :medical_code_conditions
 
   has_many :medical_therapies, through: :medical_code_therapies
